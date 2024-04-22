52fd8c45a2cb24953236cae4d67ac108a9d2b1ba
Prashant Upreti <121474134+realCoderPrashant@users.noreply.github.com>
2024-04-22 20:13:13 +0000

    Initial style for the main navigation bar

--- src/components/mainNavigation/mainNavigation.jsx
+++ src/components/mainNavigation/mainNavigation.jsx
@@ -3,14 +3,14 @@ import { navigationLogo} from "./mainNavigationItems.js";
 
 function MainNavigation() {
   return (
-    <header className="bg-gray-800 flex p-3 gap-5">
+    <header className="bg-gray-main-nav flex p-3 gap-7 mix-blend-hue m-auto px-10 py-2.5">
       {navigationLogo.map((logo) => (
-        <img  key={logo.alt} src={logo.src} alt={logo.alt} />
+        <img className="w-4 h-4" key={logo.alt} src={logo.src} alt={logo.alt} />
       ))}
       <nav>
-        <ul className="flex text-white gap-3">
-          {navigationItems.map((item) => (
-            <li key={item.name}>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</li>
+        <ul className="flex gap-7 font-medium text-xs font- font-sf">
+          {navigationItems.map((item, index) => (
+            <li key={item.name} className={index === 0 ? "text-active font-bold" : "text-inactive"}>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</li>
           ))}
         </ul>
       </nav>

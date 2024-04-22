8ce836aabe9e79c2750b2ffa260cd6cfdd2ba7eb
Prashant Upreti <121474134+realCoderPrashant@users.noreply.github.com>
2024-04-22 15:06:03 +0000

    Created the component details for main navigation

--- src/components/mainNavigation/mainNavigation.jsx
+++ src/components/mainNavigation/mainNavigation.jsx
@@ -1,1 +1,21 @@
-// insert code here
+import navigationItems from "./mainNavigationItems";
+import navigationLogo from "./mainNavigationItems";
+
+function MainNavigation() {
+  return (
+    <header>
+      {navigationLogo.map((logo) => (
+        <img  key={logo.alt} src={logo.src} alt={logo.alt} />
+      ))}
+      <nav>
+        <ul>
+          {navigationItems.map((item) => (
+            <li key={item.name}>{item.name}</li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ src/components/mainNavigation/mainNavigationItems.js
@@ -0,0 +1,17 @@
+const navigationItems = [
+  { name: "portfolio" },
+  { name: "file" },
+  { name: "edit" },
+  { name: "selection" },
+  { name: "view" },
+  { name: "go" },
+  { name: "run" },
+  { name: "terminal" },
+  { name: "window" },
+  { name: "help" },
+];
+
+const navigationLogo = [
+  { src: "../../../public/vsCode.svg", alt: "logo of Visual Studio Code" },
+];
+export default { navigationItems, navigationLogo };

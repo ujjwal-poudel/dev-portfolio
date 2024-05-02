35081b953a361e40a8e246f43ae60752a60f8103
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-02 15:50:47 -0400

    Added iconfiy dynamic icon selectors and added custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-undef
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -13,7 +15,12 @@ export default {
     },
     backgroundImage: {
       'mainBackground' : "url('/background.svg')",
-    }
+    },
+    dropShadow :{
+      'box' : '0px 0px 15px 0px rgba(97, 160, 188, 0.16)',
+    },
   },
-  plugins: [],
+  plugins: [
+    addDynamicIconSelectors(),
+  ],
 };

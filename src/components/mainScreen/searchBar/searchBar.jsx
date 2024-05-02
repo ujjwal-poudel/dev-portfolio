e104448e5a781741d789dbe78d3883854bc7c884
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-02 15:51:27 -0400

    search bar completed

---
+++ src/components/mainScreen/searchBar/searchBar.jsx
@@ -0,0 +1,11 @@
+export default function SearchBar() {
+    return (
+        <div className="search-bar">
+            <label className="relative">
+                <input type="text" placeholder="Portfolio Ujjwal" 
+                className="text-center wax-w-[558px] w-[558px] rounded-[6px] 
+                bg-[#37404C] border-[1px] border-[#989797] text-[#EFEEEE]" />
+            </label>
+        </div>
+    )
+}

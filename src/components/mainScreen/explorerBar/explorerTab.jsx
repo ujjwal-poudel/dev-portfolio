4a4c1a197a6467516dd0aec74af6eb99c7407240
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-02 22:46:17 -0400

    Created left column for the explorer

---
+++ src/components/mainScreen/explorerBar/explorerTab.jsx
@@ -0,0 +1,11 @@
+import MainTabs from "./explorerComponents/mainTabs";
+import Profile from "./explorerComponents/profile";
+
+export default function ExplorerTab() {
+    return (
+        <div className="">
+            <MainTabs />
+            <Profile />
+        </div>
+    );
+}

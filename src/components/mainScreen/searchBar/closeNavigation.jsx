3bbf53ce247378f6a644a9b16c4327325176f160
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-02 22:44:11 -0400

    Fixed the allignment and removed unused padding left

--- src/components/mainScreen/searchBar/closeNavigation.jsx
+++ src/components/mainScreen/searchBar/closeNavigation.jsx
@@ -2,7 +2,7 @@ import CloseButton from "./closeButton";
 
 export default function CloseNavigation() {
     return (
-        <div className="flex flex-row justify-center gap-[10px] pl-10px">
+        <div className="flex flex-row justify-center gap-[10px] items-center">
             <CloseButton style="bg-[#EF6A6A]" />
             <CloseButton style="bg-[#FFED8F]" />
             <CloseButton style="bg-[#75FF72]" />

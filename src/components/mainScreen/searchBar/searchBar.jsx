49f594ed4bb319ba288f97c3ec35efb519e46bc7
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-02 22:34:36 -0400

    Adjusted the width and margin for Search bar

--- src/components/mainScreen/searchBar/searchBar.jsx
+++ src/components/mainScreen/searchBar/searchBar.jsx
@@ -3,7 +3,7 @@ export default function SearchBar() {
         <div className="search-bar">
             <label className="relative">
                 <input type="text" placeholder="Portfolio Ujjwal" 
-                className="text-center wax-w-[558px] w-[558px] rounded-[6px] 
+                className="text-center wax-w-[558px] w-[430px] mr-[70px] rounded-[6px] 
                 bg-[#37404C] border-[1px] border-[#989797] text-[#EFEEEE]" />
             </label>
         </div>

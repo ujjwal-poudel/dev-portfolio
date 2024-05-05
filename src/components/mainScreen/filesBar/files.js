16d0d18c7d4046a8b4e9a91411ec50f217128a6d
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-05 13:31:07 -0400

    File names for works and other

---
+++ src/components/mainScreen/filesBar/files.js
@@ -0,0 +1,13 @@
+const regFiles = [
+    {'title' : 'Home.html'},
+    {'title' : 'About.html'},
+    {'title' : 'Contact.html'},
+]
+
+const workFiles = [
+    {'title' : 'Work1.html'},
+    {'title' : 'Work2.html'},
+    {'title' : 'Work3.html'},
+]
+
+export { regFiles, workFiles }

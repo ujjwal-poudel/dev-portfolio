9f68def8691fe2b015496c0a8f0ed76329e93b15
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-05 13:32:17 -0400

    Included Exploer and filesbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import MainNavigation from "./components/mainNavigation/mainNavigation.jsx";
 import SearchBar from "./components/mainScreen/searchBar/searchBar.jsx";
 import CloseNavigation from "./components/mainScreen/searchBar/closeNavigation.jsx";
-import MainTabs from "./components/mainScreen/explorerBar/explorerComponents/mainTabs.jsx";
+import ExplorerTab from "./components/mainScreen/explorerBar/explorerTab.jsx";
+import FilesBar from "./components/mainScreen/filesBar/filesBar.jsx";
 
 function App() {
   return (
@@ -13,18 +14,19 @@ function App() {
       <div className="bg-mainBackground bg-no-repeat w-screen bg-height bg-cover bg-center grid place-content-center">
 
         {/* Floating main screen */}
-        <div className="max-w-full w-[950px] h-[675px] border border-[#64646480] rounded-[20px] xl:w-[1200px] backdrop-blur-[28.54px] p-5 bg-[#656565]/10 shadow-box">
+        <div className="max-w-full w-[950px] h-[675px] border border-[#64646480] rounded-[20px] xl:w-[1200px] backdrop-blur-[28.54px] bg-[#656565]/10 shadow-box">
 
           {/* Search Bar */}
-          <div className="flex flex-row justify-between">
+          <div className="flex flex-row justify-between border-b-[2px] border-[#64646480] px-[15px] py-[6px]">
             <CloseNavigation />
             <SearchBar />
             <div className="invisible"></div>
           </div>
 
           {/* Content Screen */}
-          <div>
-            <MainTabs />
+          <div className="flex flex-row">
+            <ExplorerTab />
+            <FilesBar />
           </div>
         </div>
       </div>

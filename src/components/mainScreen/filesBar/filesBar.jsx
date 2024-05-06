f3d51a6b09a106b69f9d53f5ccb199e779f6557a
realCoderPrashant <ujjwal.poudel.2003@gmail.com>
2024-05-06 12:59:29 -0400

    Set up the work files and general files with styling

--- src/components/mainScreen/filesBar/filesBar.jsx
+++ src/components/mainScreen/filesBar/filesBar.jsx
@@ -5,8 +5,11 @@ export default function FilesBar() {
 
     // State for the main explorer open
     const [mainExplorerOpen, setMainExplorerOpen] = useState(true);
-    const [filesOpen, setFilesOpen] = useState("flex");
+    const [filesOpen, setFilesOpen] = useState("flex flex-col");
 
+    // State for work files
+    const [workFilesOpen, setWorkFilesOpen] = useState(false);
+    const [workFilesSection, setWorkFilesSection] = useState("flex flex-col pl-[15px]");
 
     // Function to change the main explorer open
     const changeMainExplorerOpen = () => {
@@ -14,12 +17,27 @@ export default function FilesBar() {
         changeFilesOpen();
     }
 
-    // Function to open or close the files and folders
-    const changeFilesOpen = () => {
-        setFilesOpen(mainExplorerOpen ? "hidden" : "flex pl-[2px]");
+    // Function to change the work files open
+    const changeWorkFilesOpen = () => {
+        setWorkFilesOpen(!workFilesOpen);
+        changeWorkFilesSection();
     }
 
-    const changeDownIcon = mainExplorerOpen ? "icon-[codicon--chevron-down] pr-[15px]" : "icon-[codicon--chevron-up] icon-[codicon--chevron-down] pr-[15px]";
+    // Function to open or close the files and folders
+    const changeFilesOpen = () => {
+        setFilesOpen(mainExplorerOpen ? "hidden" : "");
+    }
+
+    // Function to open or close the work files
+    const changeWorkFilesSection = () => {
+        setWorkFilesSection(workFilesOpen ? "flex flex-col pl-[15px]" : "hidden");
+    }
+
+    // Change the icon based on the main explorer open
+    const changeDownIcon = mainExplorerOpen ? "icon-[codicon--chevron-down] pr-[15px]" : "icon-[codicon--chevron-up] pr-[15px]";
+
+    // Change the icon based on the work files open
+    const changeDownIconWork = workFilesOpen ? "icon-[codicon--chevron-down] pr-[15px] text-xs h-[20px] pt-[26px]" : "rotate-90 icon-[codicon--chevron-up] pr-[15px] text-xs h-[20px] pt-[26px]";
 
     return (
         <div className="flex flex-col bg-filestab py-[17px]">
@@ -28,22 +46,36 @@ export default function FilesBar() {
                 <span className="icon-[lucide--ellipsis]"></span>
             </div>
 
-            <div className="flex flex-row text-[#FFFFFF] pl-[2px]">
-                <span className={changeDownIcon} onClick={changeMainExplorerOpen}></span>
+            <div className="flex flex-row text-[#FFFFFF] pl-[2px]" onClick={changeMainExplorerOpen}>
+                <span className={changeDownIcon}></span>
                 <h1 className="text-xs">UJJWAL&apos;S PORTFOLIO</h1>
             </div>
 
             <div className={filesOpen}>
-                <ul>
-                    {regFiles.map((file, index) => (
-                        <li key={index}>
-                            {file.title}
-                        </li>
-                    ))}
-                </ul>
-                <div className="">
-                    {workFiles.map((file, index) => (
+                <div className="text-[#FFFFFF] text-xs pl-[10px]">
+                    <ul>
+                        {regFiles.map((file, index) => (
+                            <li key={index} className="before:icon-[lucide--file] before:pl-[20px] before:text-[12px] pt-[4px]">
+                                {file.title}
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="flex flex-col">
+                        <div className="flex flex-row items-center" onClick={changeWorkFilesOpen}>
+                            <span className={changeDownIconWork}></span>
+                            <span className="icon-[lucide--folder] pr-[20px] text-[12px]"></span>
+                            <h1 className="text-xs">Work Files</h1>
+                        </div>
+                        
+                        <div className={workFilesSection}>
+                        {workFiles.map((file, index) => 
+                        (
+                            <span key={index}className="before:icon-[lucide--file] before:pl-[20px] before:text-[12px] pt-[4px]">
+                                {file.title}</span>
+                        ))
                         }
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
